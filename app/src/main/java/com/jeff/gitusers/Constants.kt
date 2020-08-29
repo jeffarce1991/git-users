f338d84cb12df48f4200d621c93abc5c8dc42cda
jeff <jeffandrew.s.arce@gmail.com>
2020-08-29 14:07:31 +0800

    Add GIT on Gateways.

--- app/src/main/java/com/jeff/gitusers/Constants.kt
+++ app/src/main/java/com/jeff/gitusers/Constants.kt
@@ -4,6 +4,7 @@ class Constants private constructor() {
     object Gateways {
         const val COVID19API = "https://api.covid19api.com"
         const val JSONPLACEHOLDER = "https://jsonplaceholder.typicode.com"
+        const val GIT = "https://api.github.com/"
     }
     object DaoExceptions {
         const val ERROR_EMPTY_RESULT = "Empty results[] from DAO request"
