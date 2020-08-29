949c73f621b1b5ffc889e995d633cb9c06cac483
jeff <jeffandrew.s.arce@gmail.com>
2020-08-29 16:14:10 +0800

    Invert Color of every 4th Avatar loaded.

--- app/src/main/java/com/jeff/gitusers/adapter/UserListAdapter.kt
+++ app/src/main/java/com/jeff/gitusers/adapter/UserListAdapter.kt
@@ -1,6 +1,8 @@
 package com.jeff.gitusers.adapter
 
 import android.content.Context
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -16,7 +18,9 @@ import com.jeff.gitusers.android.base.extension.shortToast
 import com.jeff.gitusers.database.local.User
 import com.jeff.gitusers.databinding.CustomRowBinding
 import com.jeff.gitusers.main.view.MainActivity
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+
 
 internal class UserListAdapter(
     private val context: Context,
@@ -29,6 +33,7 @@ internal class UserListAdapter(
         var itemLayout: ConstraintLayout = binding.itemLayout
         var txtTitle: TextView = binding.customRowTitle
         val coverImage: ImageView = binding.coverImage
+        val txtDetails: TextView = binding.details
 
     }
 
@@ -43,12 +48,25 @@ internal class UserListAdapter(
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val item = dataList[position]
         holder.txtTitle.text = item.login
+        holder.txtDetails.text = item.htmlUrl
         val builder = Picasso.Builder(context)
         builder.downloader(OkHttp3Downloader(context))
         builder.build().load(item.avatarUrl)
             .placeholder(R.drawable.ic_launcher_background)
             .error(R.drawable.ic_launcher_background)
-            .into(holder.coverImage)
+            .into(holder.coverImage,object: Callback{
+                override fun onSuccess() {
+                    if (position%4 == 0 && position != 0) {
+                        invertColor(holder.coverImage)
+                    }
+                }
+
+                override fun onError() {
+                    TODO("Not yet implemented")
+                }
+            })
+
+
 
         holder.itemLayout.setOnClickListener {
             val context = context as MainActivity
@@ -67,6 +85,20 @@ internal class UserListAdapter(
 
     fun getLastIndexId(): Int {
         return dataList.last().id
+    }
+
+    fun invertColor(imageView: ImageView) {
+        val invertMX = floatArrayOf(
+            -1.0f, 0.0f, 0.0f, 0.0f, 255.0f,
+            0.0f, -1.0f, 0.0f, 0.0f, 255.0f,
+            0.0f, 0.0f, -1.0f, 0.0f, 255.0f,
+            0.0f, 0.0f, 0.0f, 1.0f, 0.0f
+        )
+
+        val invertCM = ColorMatrix(invertMX)
+
+        val filter = ColorMatrixColorFilter(invertCM)
+        imageView.colorFilter = filter
     }
 
 }
