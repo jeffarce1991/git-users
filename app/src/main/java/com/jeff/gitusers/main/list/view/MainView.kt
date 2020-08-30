882c1cc9493d6317643ff04fd6bbe25adf8495d9
jeff <jeffandrew.s.arce@gmail.com>
2020-08-30 14:04:25 +0800

    Rename custom_row to item_user. Add initScrollListener. Add Search functionality.

---
+++ app/src/main/java/com/jeff/gitusers/main/list/view/MainView.kt
@@ -0,0 +1,19 @@
+package com.jeff.gitusers.main.list.view
+
+import com.hannesdorfmann.mosby.mvp.MvpView
+import com.jeff.gitusers.database.local.Photo
+import com.jeff.gitusers.database.local.User
+
+interface MainView : MvpView {
+     fun hideProgress()
+     fun showProgress()
+     fun showProgressRemote()
+     fun showProgressLocal()
+
+     fun showLoadingDataFailed()
+     fun showToast(message: String)
+     fun generateInitialUsers(users: List<User>)
+     fun generateMoreUsers(users: List<User>)
+
+     fun setSearchQueryListener(users: List<User>)
+}
