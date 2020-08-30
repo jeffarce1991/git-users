f99fd77d3067c8893f06c6cf537f345a15857f49
jeff <jeffandrew.s.arce@gmail.com>
2020-08-30 23:48:58 +0800

    Create UserDetailPresenter Interface.

---
+++ app/src/main/java/com/jeff/gitusers/main/detail/presenter/UserDetailPresenter.kt
@@ -0,0 +1,8 @@
+package com.jeff.gitusers.main.detail.presenter
+
+import com.hannesdorfmann.mosby.mvp.MvpPresenter
+import com.jeff.gitusers.main.detail.view.UserDetailView
+
+interface UserDetailPresenter : MvpPresenter<UserDetailView> {
+
+}
