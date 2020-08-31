cd658a392251acbc8c5395fd519c33d05bf3e5a0
jeff <jeffandrew.s.arce@gmail.com>
2020-08-31 08:57:19 +0800

    Add UserDetailActivity in MainModule.

--- app/src/main/java/com/jeff/gitusers/main/MainModule.kt
+++ app/src/main/java/com/jeff/gitusers/main/MainModule.kt
@@ -1,6 +1,8 @@
 package com.jeff.gitusers.main
 
 import com.jeff.gitusers.ActivityScope
+import com.jeff.gitusers.main.detail.presenter.UserDetailsPresenterModule
+import com.jeff.gitusers.main.detail.view.UserDetailsActivity
 import com.jeff.gitusers.main.list.presenter.MainPresenterModule
 import com.jeff.gitusers.main.list.view.MainActivity
 import dagger.Module
@@ -12,4 +14,8 @@ interface MainModule {
     @ActivityScope
     @ContributesAndroidInjector(modules = [MainPresenterModule::class])
     fun mainActivity(): MainActivity
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [UserDetailsPresenterModule::class])
+    fun userDetailActivity(): UserDetailsActivity
 }
