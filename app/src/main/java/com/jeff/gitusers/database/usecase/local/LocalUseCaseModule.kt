4231f237eab34b21a512bc0488192d571c59f0d5
jeff <jeffandrew.s.arce@gmail.com>
2020-08-31 18:45:16 +0800

    Binds UserDetails LocalSaver and LocalLoader.

--- app/src/main/java/com/jeff/gitusers/database/usecase/local/LocalUseCaseModule.kt
+++ app/src/main/java/com/jeff/gitusers/database/usecase/local/LocalUseCaseModule.kt
@@ -1,8 +1,12 @@
 package com.jeff.gitusers.database.usecase.local
 
+import com.jeff.gitusers.database.usecase.local.loader.DefaultUserDetailsLocalLoader
 import com.jeff.gitusers.database.usecase.local.loader.DefaultUserLocalLoader
+import com.jeff.gitusers.database.usecase.local.loader.UserDetailsLocalLoader
 import com.jeff.gitusers.database.usecase.local.loader.UserLocalLoader
+import com.jeff.gitusers.database.usecase.local.saver.DefaultUserDetailsLocalSaver
 import com.jeff.gitusers.database.usecase.local.saver.DefaultUserLocalSaver
+import com.jeff.gitusers.database.usecase.local.saver.UserDetailsLocalSaver
 import com.jeff.gitusers.database.usecase.local.saver.UserLocalSaver
 import dagger.Binds
 import dagger.Module
@@ -14,4 +18,10 @@ interface LocalUseCaseModule {
 
     @Binds
     fun bindUserLocalSaver(implementation: DefaultUserLocalSaver): UserLocalSaver
+
+    @Binds
+    fun bindUserDetailsLocalLoader(implementation: DefaultUserDetailsLocalLoader): UserDetailsLocalLoader
+
+    @Binds
+    fun bindUserDetailsLocalSaver(implementation: DefaultUserDetailsLocalSaver): UserDetailsLocalSaver
 }
