c4ac4af89f834dd4f2f102e82eea84ce2fda3a88
jeff <jeffandrew.s.arce@gmail.com>
2020-08-31 09:14:03 +0800

    Increment DB version.

--- app/src/main/java/com/jeff/gitusers/database/room/AppDatabase.java
+++ app/src/main/java/com/jeff/gitusers/database/room/AppDatabase.java
@@ -6,16 +6,19 @@ import androidx.room.TypeConverters;
 
 import com.jeff.gitusers.database.local.Photo;
 import com.jeff.gitusers.database.local.User;
+import com.jeff.gitusers.database.local.UserDetails;
 import com.jeff.gitusers.database.room.converter.PhotoConverter;
 import com.jeff.gitusers.database.room.converter.UserConverter;
+import com.jeff.gitusers.database.room.converter.UserDetailsConverter;
 import com.jeff.gitusers.database.room.dao.PhotoDao;
 import com.jeff.gitusers.database.room.dao.UserDao;
 
 @Database(
         entities = {
-                User.class
+                User.class,
+                UserDetails.class
         },
-        version = 5,
+        version = 14,
         exportSchema = false
 )
 
