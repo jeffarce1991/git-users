5747f58c93ee4e72eb7332a382dd33ba50c66202
jeff <jeffandrew.s.arce@gmail.com>
2020-08-31 08:59:02 +0800

    Add loadUserDetails(login: String) in UserRemoteLoader interface.

--- app/src/main/java/com/jeff/gitusers/webservices/usecase/loader/UserRemoteLoader.kt
+++ app/src/main/java/com/jeff/gitusers/webservices/usecase/loader/UserRemoteLoader.kt
@@ -1,6 +1,7 @@
 package com.jeff.gitusers.webservices.usecase.loader
 
 import com.jeff.gitusers.webservices.dto.PhotoDto
+import com.jeff.gitusers.webservices.dto.UserDetailsDto
 import com.jeff.gitusers.webservices.dto.UserDto
 import io.reactivex.Single
 
@@ -8,4 +9,6 @@ interface UserRemoteLoader {
 
     fun loadInitialUsers(): Single<List<UserDto>>
     fun loadMoreUsers(id: Int): Single<List<UserDto>>
+
+    fun loadUserDetails(login: String): Single<UserDetailsDto>
 }
